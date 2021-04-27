f315d2d8c725834cf90334161a16f4ef04116408
amili <amilily0710@naver.com>
2021-04-27 09:51:48 +0900

    Hello kotlin initial commit

---
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+   // var btnHello : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    val btnHello = findViewById<Button>(R.id.btnhello)
+    btnHello.setOnClickListener {
+        Toast.makeText(this@MainActivity, "Hello Kotlin", Toast.LENGTH_LONG).show()
+    }
+    }
+}
